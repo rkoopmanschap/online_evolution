3c70d8ec2068bc53c4071921a59ef5ad3977b02b
Richard Koopmanschap <rkoopmanschap@gmail.com>
2018-10-16 10:47:53 +0200

    Updated main flow of application

--- online_evolution.rb
+++ online_evolution.rb
@@ -20,11 +20,57 @@ class OnlineEvolution
 	end
 
 	def process_generation
-
+		GENERATION_DURATION.times do
+			animal_array = generate_animal_order
+			animal_array.each do |animal|
+				action = animal.pick_action
+				perform_action(animal, action)
+			end
+		end
 	end
 
 	def process_evolution
+		# for each species, get all surviving animals
+		# calculate fitness
+		# select parents
+		# recombine
+		# mutate
+	end
 
+	def generate_animal_order
+		result = []
+
+		# put all animals in array
+		# shuffle
+		# OR
+		# shuffle while filling array
+
+		return result
+	end
+
+	def perform_action(animal, action)
+		case action
+		when :gather_plants
+			process_gather(animal, :plants)
+		when :gather_insects
+			process_gather(animal, :insects)
+		when :gather_fish
+			process_gather(animal, :fish)
+		when :attack
+			process_attack(animal)
+		end
+	end
+
+	def process_gather(animal, type)
+		# calculate_result
+		# update animal
+		# update type availability
+	end
+
+	def process_attack(animal)
+		# select target
+		# calculate_result
+		# update animals
 	end
 
 	# main loop
